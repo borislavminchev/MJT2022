36f773d6bbd99727ca8d253af60e246ba70d1191
borislavminchev <59176170+borislavminchev@users.noreply.github.com>
2021-10-30 12:01:46 +0300

    changed some stuff

--- Spotify/src/bg/sofia/uni/fmi/mjt/spotify/Main.java
+++ Spotify/src/bg/sofia/uni/fmi/mjt/spotify/Main.java
@@ -1,8 +1,14 @@
 package bg.sofia.uni.fmi.mjt.spotify;
 
+import bg.sofia.uni.fmi.mjt.spotify.playable.Playable;
+import bg.sofia.uni.fmi.mjt.spotify.playlist.Playlist;
+import bg.sofia.uni.fmi.mjt.spotify.playlist.UserPlaylist;
+
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("opa");
+        System.out.println("Hello");
     }
 }
