e2bed30346682ceb8394f0b1a7f8674bc2a1d1b9
borislavminchev <59176170+borislavminchev@users.noreply.github.com>
2021-10-30 11:59:37 +0300

    Added implementation to UserPlaylist

--- Spotify/src/bg/sofia/uni/fmi/mjt/spotify/playlist/UserPlaylist.java
+++ Spotify/src/bg/sofia/uni/fmi/mjt/spotify/playlist/UserPlaylist.java
@@ -3,17 +3,39 @@ package bg.sofia.uni.fmi.mjt.spotify.playlist;
 import bg.sofia.uni.fmi.mjt.spotify.exceptions.PlaylistCapacityExceededException;
 import bg.sofia.uni.fmi.mjt.spotify.playable.Playable;
 
+import java.util.Arrays;
+
 public class UserPlaylist implements Playlist {
 
-    public UserPlaylist(String name) {}
+    private String name;
+    private Playable[] contentInPlaylist;
+    private int currentSize;
+
+    private final int MAX_SIZE = 20;
+
+
+    public UserPlaylist(String name) {
+        this.name = name;
+        contentInPlaylist = new Playable[MAX_SIZE];
+        this.currentSize = 0;
+    }
 
     @Override
     public void add(Playable playable) throws PlaylistCapacityExceededException {
+        if(playable == null) {
+            throw new IllegalArgumentException("Cannot add null as content in playlist");
+        }
 
+        if(currentSize >= this.contentInPlaylist.length) {
+            throw new PlaylistCapacityExceededException("Capacity Exceeded. Max capacity is " + MAX_SIZE);
+        }
+
+        this.contentInPlaylist[currentSize] = playable;
+        this.currentSize++;
     }
 
     @Override
     public String getName() {
-        return null;
+        return this.name;
     }
 }
