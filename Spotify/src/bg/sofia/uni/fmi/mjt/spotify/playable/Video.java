3bb5c0648101d9708113afa89648534338314c08
borislavminchev <59176170+borislavminchev@users.noreply.github.com>
2021-10-30 11:58:34 +0300

    Added implementation to playables

--- Spotify/src/bg/sofia/uni/fmi/mjt/spotify/playable/Video.java
+++ Spotify/src/bg/sofia/uni/fmi/mjt/spotify/playable/Video.java
@@ -3,33 +3,49 @@ package bg.sofia.uni.fmi.mjt.spotify.playable;
 import bg.sofia.uni.fmi.mjt.spotify.playable.Playable;
 
 public class Video implements Playable {
+    private String title;
+    private String artist;
+    private int year;
+    private double duration;
+
+    private static int totalPlays = 0;
+
+    public Video(String title, String artist, int year, double duration) {
+        this.title = title;
+        this.artist = artist;
+        this.year = year;
+        this.duration = duration;
+    }
+
     @Override
     public String play() {
-        return null;
+        totalPlays++;
+        return "Currently playing" + "VIDEO"  + "content: " + title + " - " + artist;
     }
 
     @Override
     public int getTotalPlays() {
-        return 0;
+        return totalPlays;
     }
 
     @Override
     public String getTitle() {
-        return null;
+        return this.title;
     }
 
     @Override
     public String getArtist() {
-        return null;
+        return this.artist;
     }
 
     @Override
     public int getYear() {
-        return 0;
+        return this.year;
     }
 
     @Override
     public double getDuration() {
-        return 0;
+        return this.duration;
     }
+
 }
