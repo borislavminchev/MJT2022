07c73e4c7de59f13230dd63703e6a42f43214065
borislavminchev <59176170+borislavminchev@users.noreply.github.com>
2021-10-31 19:31:34 +0200

    Added code to Spotify.java methods

--- Spotify/src/bg/sofia/uni/fmi/mjt/spotify/Spotify.java
+++ Spotify/src/bg/sofia/uni/fmi/mjt/spotify/Spotify.java
@@ -1,41 +1,103 @@
 package bg.sofia.uni.fmi.mjt.spotify;
 
 import bg.sofia.uni.fmi.mjt.spotify.account.Account;
+import bg.sofia.uni.fmi.mjt.spotify.account.AccountType;
 import bg.sofia.uni.fmi.mjt.spotify.exceptions.AccountNotFoundException;
 import bg.sofia.uni.fmi.mjt.spotify.exceptions.PlayableNotFoundException;
+import bg.sofia.uni.fmi.mjt.spotify.exceptions.PlaylistCapacityExceededException;
 import bg.sofia.uni.fmi.mjt.spotify.exceptions.StreamingServiceException;
 import bg.sofia.uni.fmi.mjt.spotify.playable.Playable;
 
 public class Spotify implements StreamingService{
-    public Spotify(Account[] accounts, Playable[] playableContent){}
+
+    private Account[] registeredAccounts;
+    private Playable[] playableContent;
+
+    public Spotify(Account[] accounts, Playable[] playableContent){
+        this.registeredAccounts = accounts;
+        this.playableContent = playableContent;
+    }
 
     @Override
     public void play(Account account, String title) throws AccountNotFoundException, PlayableNotFoundException {
+        if (account == null || title == null || title.isEmpty()) {
+            throw new IllegalArgumentException("Invalid argument was passed to method play");
+        }
 
+        if(!isAccountInPlatform(account)) {
+            throw new AccountNotFoundException("Account with username: " + account.getEmail() + "was not registered in the platform");
+        }
+
+        account.listen(findByTitle(title));
     }
 
     @Override
     public void like(Account account, String title) throws AccountNotFoundException, PlayableNotFoundException, StreamingServiceException {
+        if (account == null || title == null || title.isEmpty()) {
+            throw new IllegalArgumentException("Invalid argument was passed to method like");
+        }
+        if(!isAccountInPlatform(account)) {
+            throw new AccountNotFoundException("Account with username: " + account.getEmail() + "was not registered in the platform");
+        }
 
+        try {
+            account.getLibrary().getLiked().add(findByTitle(title));
+        } catch (PlaylistCapacityExceededException e) {
+            throw new StreamingServiceException("Couldn't add " + title + "to account's liked playlist." + e.getMessage());
+        }
     }
 
     @Override
     public Playable findByTitle(String title) throws PlayableNotFoundException {
-        return null;
+        for (int i = 0; i < this.playableContent.length; i++) {
+            if(this.playableContent[i].getTitle().equals(title)) {
+                return this.playableContent[i];
+            }
+        }
+
+       throw new PlayableNotFoundException("Playable with title: " + title + " was not registered in the platform");
     }
 
     @Override
     public Playable getMostPlayed() {
-        return null;
+        Playable mostPlayed = this.playableContent[0];
+        for (int i = 1; i < this.playableContent.length; i++) {
+           if(mostPlayed.getTotalPlays() < this.playableContent[i].getTotalPlays()) {
+               mostPlayed = this.playableContent[i];
+           }
+        }
+        return mostPlayed;
     }
 
     @Override
     public double getTotalListenTime() {
-        return 0;
+        double total = 0;
+        for (int i = 0; i < this.playableContent.length; i++) {
+            total += this.playableContent[i].getDuration();
+        }
+        return total;
     }
 
     @Override
     public double getTotalPlatformRevenue() {
-        return 0;
+        double totalRevenue = 0;
+        for (int i = 0; i < this.registeredAccounts.length; i++) {
+            if(this.registeredAccounts[i].getType() == AccountType.FREE) {
+                totalRevenue += this.registeredAccounts[i].getAdsListenedTo() * 0.1;
+            }else if (this.registeredAccounts[i].getType() == AccountType.PREMIUM) {
+                totalRevenue += 25.0;
+            }
+        }
+        return totalRevenue;
+    }
+
+    public boolean isAccountInPlatform(Account account) {
+        for (int i = 0; i < this.registeredAccounts.length; i++) {
+            if(this.registeredAccounts[i].equals(account)){
+               return true;
+            }
+        }
+
+        return false;
     }
 }
