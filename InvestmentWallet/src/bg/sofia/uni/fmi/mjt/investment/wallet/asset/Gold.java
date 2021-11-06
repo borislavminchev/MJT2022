20a1deaecda7638caf950b7ea904f49c019364ef
borislavminchev <59176170+borislavminchev@users.noreply.github.com>
2021-11-06 10:40:21 +0200

    Added code implementation

--- InvestmentWallet/src/bg/sofia/uni/fmi/mjt/investment/wallet/asset/Gold.java
+++ InvestmentWallet/src/bg/sofia/uni/fmi/mjt/investment/wallet/asset/Gold.java
@@ -1,21 +1,26 @@
 package bg.sofia.uni.fmi.mjt.investment.wallet.asset;
 
 public class Gold implements Asset {
+    private final String id;
+    private final String name;
+
     public Gold(String id, String name) {
+        this.id = id;
+        this.name = name;
     }
 
     @Override
     public String getId() {
-        return null;
+        return this.id;
     }
 
     @Override
     public String getName() {
-        return null;
+        return this.name;
     }
 
     @Override
     public AssetType getType() {
-        return null;
+        return AssetType.GOLD;
     }
 }
