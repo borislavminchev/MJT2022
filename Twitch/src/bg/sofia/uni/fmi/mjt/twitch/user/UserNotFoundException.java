4d988046d7b45f41f2ca8412872582ef94138942
borislavminchev <59176170+borislavminchev@users.noreply.github.com>
2021-11-20 17:41:33 +0200

    Added exceptions

--- Twitch/src/bg/sofia/uni/fmi/mjt/twitch/user/UserNotFoundException.java
+++ Twitch/src/bg/sofia/uni/fmi/mjt/twitch/user/UserNotFoundException.java
@@ -1,6 +1,6 @@
 package bg.sofia.uni.fmi.mjt.twitch.user;
 
-public class UserNotFoundException extends Throwable{
+public class UserNotFoundException extends Throwable {
     public UserNotFoundException(String message) {
         super(message);
     }
--- Twitch/src/bg/sofia/uni/fmi/mjt/twitch/user/UserStreamingException.java
+++ Twitch/src/bg/sofia/uni/fmi/mjt/twitch/user/UserStreamingException.java
@@ -1,5 +1,7 @@
 package bg.sofia.uni.fmi.mjt.twitch.user;
 
-public class UserStreamingException extends Throwable{
-
+public class UserStreamingException extends Throwable {
+    public UserStreamingException(String message) {
+        super(message);
+    }
 }
